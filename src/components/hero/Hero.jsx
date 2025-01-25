1b7b88dee52e1eec46b83c0f1f5ccec97e907ed6
unknown <adilsatuva@gmail.com>
2025-01-25 13:16:12 +0600

    adil

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -1,10 +1,19 @@
-import React from 'react'
-import './Hero.scss'
+import "./Hero.scss";
 
 function Hero() {
   return (
-    <div>Hero</div>
-  )
+    <div className="Hero">
+      <div className="container">
+        <h1 className="hero-title">
+          Вместе мы сделаем много полезного для природы и человека!
+          <p className="hero-subtitle">
+            EcoBishkek - платформа для отслеживания загрязнений,подачи <br />{" "}
+            жалоб и участия в экологических инициативах.
+          </p>
+        </h1>
+      </div>
+    </div>
+  );
 }
 
-export default Hero
+export default Hero;
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,25 +1,22 @@
-import React from 'react'
-import './Home.scss'
-import Hero from '../../components/hero/Hero'
-import About from '../../components/about/About'
-import Active from '../../components/active/Active'
-import Events from '../../components/events/Events'
-import Karta from '../../components/Karta/Karta'
-import Sponsor from '../../components/sponsor/Sponsor'
+import "./Home.scss";
+import Hero from "../../components/hero/Hero";
+import About from "../../components/about/About";
+import Active from "../../components/active/Active";
+import Events from "../../components/events/Events";
+import Karta from "../../components/Karta/Karta";
+import Sponsor from "../../components/sponsor/Sponsor";
 
 function Home() {
   return (
-    <section className='home'>
-      <div className='container'>
-        <Hero />
-        <About />
-        <Active />
-        <Events />
-        <Karta />
-        <Sponsor />
-      </div>
+    <section className="home">
+      <Hero />
+      <About />
+      <Active />
+      <Events />
+      <Karta />
+      <Sponsor />
     </section>
-  )
+  );
 }
 
-export default Home
+export default Home;
