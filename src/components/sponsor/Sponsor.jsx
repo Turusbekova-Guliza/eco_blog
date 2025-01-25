ee09ffa41c5be2485eac43b9f1e28dcd7c49a393
nurs <test@gmail.com>
2025-01-25 16:02:24 +0600

    Footer

--- src/components/sponsor/Sponsor.jsx
+++ src/components/sponsor/Sponsor.jsx
@@ -5,7 +5,28 @@ function Sponsor() {
   return (
     <div>
       <h2 className='podderjka'>Нас поддерживают</h2>
+
+      <div className="sponsor-images">
+        <img src="src/components/sponsor/img/castle.png" alt="Sponsor 1" className="sponsor-image" />
+        <img src="src/components/sponsor/img/ecocup.png" alt="Sponsor 2" className="sponsor-image" />
+        <img src="src/components/sponsor/img/ololo.png" alt="Sponsor 3" className="sponsor-image" />
+        <img src="src/components/sponsor/img/ProHost.png" alt="Sponsor 4" className="sponsor-image" />
+        <img src="src/components/sponsor/img/silkwayairlines.png" alt="Sponsor 5" className="sponsor-image" />
+        <img src="src/components/sponsor/img/ta.png" alt="Sponsor 6" className="sponsor-image" />
+      </div>
+
+
+
+
+
+
+
+
+
     </div>
+
+
+
   )
 }
 
